168a1c4fdec1faaf9a151a6a970988a676ef6bde
Valentina-K <kvalentyna@hotmail.com>
2023-01-16 16:06:19 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,8 +5,8 @@ import ImageGallery from './ImageGallery/ImageGallery';
 import Modal from './Modal/Modal';
 import Searchbar from './Searchbar/Searchbar';
 
-const BASE_URL = 'https://pixabay.com/api/';
-const KEY = '32106201-0c90331702e18f870e8d36f12';
+//const BASE_URL = 'https://pixabay.com/api/';
+//const KEY = '32106201-0c90331702e18f870e8d36f12';
 
 export default class App extends Component {
   state = {
